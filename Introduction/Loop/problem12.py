570b829b9daa1019e7647b30439862af36b2acbe
Trisha-SWE <your_github_rukaiyatrisha.swe@gmail.com>
2026-07-24 00:44:26 +0600

    Add solution

---
+++ Introduction/Loop/problem12.py
@@ -0,0 +1,22 @@
+#for loop 
+#list for item
+
+# for item in ['Egg','Raice','oil']:
+#     print (item)
+    
+# for number in range (1,6):
+#     print (number)
+       
+       
+       
+       #bills
+       # 
+    
+    
+bills = [10, 30, 50 ,10]
+total = 0
+for bill in bills:
+    total += bill
+print(f"Total: {total}")
+        
+    
